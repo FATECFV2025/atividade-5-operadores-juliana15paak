4aa254e64ac4df8df1d85c8a9499c924a98d8614
juliana15paak <fvjulianaasilva976@gmail.com>
2025-11-05 17:58:28 -0300

    Adiciona método para calcular expressões matemáticas

--- FazerVsCode/src/Operadores.java
+++ FazerVsCode/src/Operadores.java
@@ -15,5 +15,40 @@ public class Operadores {
     return v1 / v2;
   }
 
-  
+  //Operadores de Atribuição
+  //A conta será realizada na ordem que o usuário digitou
+  public void expressao (){
+    System.out.print("Digite um número, uma operação (+, -, *, /) e outro número, dando Enter entre cada um. Continue digitando sua expressão, para parar, digite '=': ");
+    float resultado = in.nextFloat();
+    while (true) {
+        String o = in.next();
+        if (o.equals("=")) {
+            break; // Sai do loop
+        }
+        float v = in.nextFloat();
+
+        // Verificação de operadores
+        if (o.equals("+")){
+            resultado += v; // Operador de atribuição (adição)
+        }
+        else if (o.equals("-")){
+            resultado -= v; // Operador de atribuição (subtração)
+        }
+        else if (o.equals("*")){
+            resultado *= v; // Operador de atribuição (multiplicação)
+        }
+        else if (o.equals("/")){
+            if (v == 0) {
+                System.out.println("Erro: Divisão por zero. Operação ignorada.");
+            } else {
+                resultado /= v; // Operador de atribuição (divisão)
+            }
+        }
+        else {
+            System.out.println("Operador inválido. Tente novamente.");
+        }
+    }
+    
+    System.out.println("Resultado da expressão: "+ resultado);
+  }
 }
